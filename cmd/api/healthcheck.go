861ee33437f8d8dd74910034b14fb9ef1265a11c
lipandr <andrey.lipayev@gmail.com>
2023-08-22 16:31:00 +0300

    API Endpoints and RESTFul Routing

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -5,9 +5,9 @@ import (
 	"net/http"
 )
 
-func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+func (app *application) healthcheckHandler(w http.ResponseWriter, _ *http.Request) {
+	_, _ = fmt.Fprintln(w, "status: available")
+	_, _ = fmt.Fprintf(w, "environment: %s\n", app.config.env)
+	_, _ = fmt.Fprintf(w, "version: %s\n", version)
 
 }
