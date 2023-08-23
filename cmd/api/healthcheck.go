c971c94b31d45642c2df66110debc61446d27604
lipandr <andrey.lipayev@gmail.com>
2023-08-23 15:38:18 +0300

    JSON encoding + Enveloping responses

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,13 +1,23 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, _ *http.Request) {
-	_, _ = fmt.Fprintln(w, "status: available")
-	_, _ = fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	_, _ = fmt.Fprintf(w, "version: %s\n", version)
+	data := envelope{
+		"status": "available",
+		"system info": map[string]string{
+			"environment": app.config.env,
+			"version":     version,
+		},
+	}
 
+	err := app.writeJSON(w, http.StatusOK, data, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request",
+			http.StatusInternalServerError)
+		return
+	}
 }
